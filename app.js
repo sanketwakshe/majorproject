83f9ff3e717859ee1f73486f94fd1b13d6ad1175
sanketwakshe <153482871+sanketwakshe@users.noreply.github.com>
2024-07-31 21:37:07 +0530

    commit

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
     mongoUrl:dbUrl,
     crypto:{
-        secret:process.env.SECRET,
+        secret:process.env.SECRET
     },
     touchAfter:24*3600,
  });
